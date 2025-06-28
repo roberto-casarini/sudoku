d1bab18383b9be910891d50d92616e750c492fef
Roberto <roberto.casarini@gmail.com>
2025-06-28 18:37:44 +0200

    Fixed some styling

--- resources/views/livewire/single-cell.blade.php
+++ resources/views/livewire/single-cell.blade.php
@@ -8,9 +8,11 @@
     @endif
 
     <div @class([
-        'w-12 h-12 border-1 border-gray-300',
+        'w-12 h-12',
         '!border-l-2 !border-l-gray-600' => $this->hasBorder('left'),
+        '!border-l-1 !border-l-gray-300' => !$this->hasBorder('left'),
         '!border-t-2 !border-t-gray-600' => $this->hasBorder('top'),
+        '!border-t-1 !border-t-gray-300' => !$this->hasBorder('top'),
         '!border-r-2 !border-r-gray-600' => $this->hasBorder('right'),
         '!border-b-2 !border-b-gray-600' => $this->hasBorder('bottom'),
     ]) wire:click="setEdit" >
@@ -26,20 +28,11 @@
             </div>
         @else
             <div @class([
-                'text-3xl text-center my-1',
+                'text-3xl text-center -my-1',
                 'text-red-600' => !$preSetting
             ])>
                 {{ $cellValue }}
             </div>
         @endif
     </div>
-
-    @if($edit)
-    <?php
-        $cell = $this->cellToText;
-    ?>
-    <div x-cloak class="absolute -top-5 -left-5 z-10" x-transition.opacity.duration.500ms>
-        <livewire:select-number :multiple=$showPossibilities :cell=$cell />
-    </div>
-    @endif
 </div>
